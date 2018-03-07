8e1ef21ef407edae644165d547bec1912a783806
jjsull1van <jim@arcisphere.com>
2018-03-07 12:31:45 -0500

    Completed update on workitem 5841
    Completed update on workitem 5841

--- item/HelloWorld.java
+++ item/HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println("Hello, my name is Jimmy!");
-       
+        System.out.println("I will be your virtual assistant!");
+        System.out.println("Please, be my guest");
+        //System.out.println("this won't be printed!")
+        //this is a comment
         System.exit(0); //another comment
     }
 }
