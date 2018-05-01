d9c4c1b80a56122ed0b4cd5712e2d2c93e5fced6
jjsull1van <jim@arcisphere.com>
2018-05-01 10:59:05 -0400

    Update workitem 13682

--- item/HelloWorld.java
+++ item/HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
     public static void main( String[] args ) {
-        System.out.println("Hello, my name is Jimmy!");
+        ///System.out.println("Hello, my name is Jimmy!");
         System.out.println("I will be your virtual assistant!");
         System.out.println("Please, be my guest");
-		System.out.println("Please, test");
+		///System.out.println("Please, test");
         System.out.println("this won't be printed!")
 		System.out.println('Hello');
 		Systen.out.println("Git Demo");
